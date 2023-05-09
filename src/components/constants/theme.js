088bb17b3f63e96761cc4c2c3e491f1aee4fffb7
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-05-09 22:07:49 +0300

    Adds a constants

---
+++ src/components/constants/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    accentColor: '#008cff',
+    sectionCard: '#f6f5f3',
+    text: '#423d37',
+    textTitle: '#8b6446',
+    btnBorderColor: '#06060680',
+    bgContainer: 'rgb(233, 233, 233);',
+    shadowContainer: '12px 17px 51px rgba(0,0,0,.22)',
+    boxShadow:
+      'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+    textShadow:
+      '-0.6px 0 0.7px #d287c3, -1.2px 0 1.4px #88c1cf,-1.8px 0 2.1px #cbd798, -2.4px 0 2.8px #c570b5, -3px 0 3.5px #67b4c6, -3.6px 0 4.2px #ced8a4, -4.2px 0 4.9px #dea4d3, -4.8px 0 5.6px #a6cfd9, -5.4px 0 6.3px #b4c471, -6px 0 7px #daadd2;',
+    cubicBezier: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
